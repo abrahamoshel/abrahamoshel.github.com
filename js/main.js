8ae6849e204d1a06f0be3befeb0ef9e2dcaa41c7
Abraham Oshel <abraham.oshel@gmail.com>
2012-09-25 01:46:53 -0500

    map and countdown adjustments

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  * J-Day -- HTML prenium template
  *
  * Copyright (c) 2012 F²
- * 
+ *
  * Main Javascript
  */
 /********************************/
@@ -14,7 +14,7 @@ jQuery.noConflict()(function($){
     * Set your date here  (YEAR, MONTH (0 for January/11 for December), DAY, HOUR, MINUTE, SECOND)
     * according to the GMT+0 Timezone
     **/
-    var launch = new Date(2013, 06, 14, 11, 00);
+    var launch = new Date(2012, 10, 25, 11, 00);
     /**
     * The script
     **/
@@ -23,7 +23,7 @@ jQuery.noConflict()(function($){
     var hours = $('#hours');
     var minutes = $('#minutes');
     var seconds = $('#seconds');
-    
+
     setDate();
     function setDate(){
         var now = new Date();
@@ -53,7 +53,7 @@ jQuery.noConflict()(function($){
 
             message.html('Welcome ! Unfortunately, we’re not ready now. But, the launch day is coming !');
         }
-    } 
+    }
 	});
 });
 /********************************/
@@ -62,8 +62,8 @@ jQuery.noConflict()(function($){
 jQuery.noConflict()(function($){
 	$(document).ready(function() {
 		$(".show_progress_area").click(function(){
-				$('#progress_area').slideToggle(); 
-				$('#open_me_progress').hide(); 
+				$('#progress_area').slideToggle();
+				$('#open_me_progress').hide();
 				$('#button_open_progress').css("marginBottom", "24px");
 		});
     });
@@ -110,45 +110,45 @@ jQuery.noConflict()(function($){
         $('#contact_area').slideToggle(function initialize() {
             /********************************/
             /*Map -- Thank to the Google map API -> https://developers.google.com/maps/
-            /********************************/	
+            /********************************/
             var infoMap = new google.maps.InfoWindow();
-            var Mappos = new google.maps.LatLng(40.714353,-74.005973);
+            var Mappos = new google.maps.LatLng(41.88691, -87.64315);
             var pictoMap = 'images/picto_map.png';
             var pictoMapShadow = 'images/picto_map_shadow.png';
             var contentMap = '<div class="infoBulle" ><h4>Custom Google map !</h4><p>Area for you address or a description</p></div>';
             var options = {
                     center: Mappos,
-                    zoom: 14,					
+                    zoom: 14,
                     mapTypeId: google.maps.MapTypeId.ROADMAP,
                     mapTypeControl: true,
                     mapTypeControlOptions: {
                     style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
-                    },					
+                    },
                     navigationControl: true,
-                    navigationControlOptions: {					
+                    navigationControlOptions: {
                     style: google.maps.NavigationControlStyle.SMALL
                     }
-            };			
+            };
             var carte = new google.maps.Map(document.getElementById("GoogleMaps"), options);
             var iconMap = new google.maps.Marker({
                     position: Mappos,
                     map: carte,
                     icon : pictoMap,
-                    shadow : pictoMapShadow});												
+                    shadow : pictoMapShadow});
 
             google.maps.event.addListener(iconMap, 'click', function() {
                     infoMap.setContent(contentMap);
                     infoMap.open(carte, this);
-                    carte.panTo(Mappos);});		
+                    carte.panTo(Mappos);});
             google.maps.event.addListener(carte, 'click', function() {
             carte.panTo(Mappos);infoMap.setContent(contentMap);infoMap.open(carte, iconMap);
-            });			
+            });
 
        });
-       $('#open_me_contact').hide(); 
+       $('#open_me_contact').hide();
        $('#additional').css("marginBottom", "0px");
        $('#button_open_contact').css("marginBottom", "12px");
-    });	
+    });
     // For the forms alerts
     $(".alert").alert('closed')
 	});
